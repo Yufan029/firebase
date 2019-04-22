c617511d6edf854918fa8ef490d6edd5f595feee
Yufan Zhang <zhang.yufan029@gmail.com>
2019-04-22 10:43:36 +1000

    add onsnapshot method to update the UI in real-time.

--- firestore/app.js
+++ firestore/app.js
@@ -4,20 +4,68 @@ function renderData(doc) {
     const li = document.createElement('li');
     const name = document.createElement('span');
     const city = document.createElement('span');
+    const div = document.createElement('div');
+    const icon = document.createElement('i');
 
-    li.setAttribute('data-sid', doc.id);
+    li.setAttribute('data-id', doc.id);
 
+    
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    icon.className = 'material-icons blue';
+    icon.textContent = 'delete'
     
     li.appendChild(name);
     li.appendChild(city);
 
+    div.appendChild(icon);
+    li.appendChild(div);
+
     ulElement.appendChild(li);
+
+    icon.addEventListener('click', (e) => {
+        e.stopPropagation();
+        db.collection('cafes').doc(li.getAttribute('data-id')).delete();
+    })
 }
 
-db.collection('cafes').get().then(snapchsot => {
-    snapchsot.docs.forEach(doc => {
-        renderData(doc)
-    });
+// db.collection('cafes').orderBy('name').get().then(snapchsot => {
+//     snapchsot.docs.forEach(doc => {
+//         renderData(doc)
+//     });
+// })
+
+const form = document.querySelector('#add-cafe-form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let cafeName = document.querySelector('#cafe-name');
+    let cafeCity = document.querySelector('#cafe-city');
+    console.log(cafeName.value);
+    db.collection('cafes').add({
+        name: cafeName.value,
+        city: cafeCity.value
+    })
+    .then((docRef) => {
+        console.log('add data successful, doc ref id: ' + docRef.id);
+    })
+    .catch(e => {
+        console.log('Error: ' + e)
+    })
+
+    cafeName.value = cafeCity.value = '';
+
+})
+
+db.collection('cafes').onSnapshot(snapshot => {
+    // console.log(snapshot.docChanges());    
+    // snapshot.docs.forEach(doc => console.log(doc));
+
+    snapshot.docChanges().forEach(change => {
+        if (change.type === 'added') {
+            renderData(change.doc);
+        } else if (change.type === 'removed') {
+            const li = ulElement.querySelector('[data-id=' + change.doc.id + ']');
+            ulElement.removeChild(li);
+        }
+    })
 })
